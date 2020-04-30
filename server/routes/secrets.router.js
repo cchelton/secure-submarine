309d1804b8d014a727abac02693824f09fad4677
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-30 11:36:58 -0500

    adds clearance lvl to query

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -7,8 +7,11 @@ const {
 
 router.get("/", rejectUnauthenticated, (req, res) => {
   console.log("req.user:", req.user);
+  const userClearanceLvl = req.user.clearance_level;
+  const queryText = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`;
+
   pool
-    .query('SELECT * FROM "secret";')
+    .query(queryText, [userClearanceLvl])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log("Error making SELECT for secrets:", error);
