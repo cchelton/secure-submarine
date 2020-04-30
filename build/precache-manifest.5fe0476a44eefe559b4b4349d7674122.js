18d03a6f39a7a3ae8723fa4262e8c61fd19bbf24
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-30 13:23:51 -0500

    npm run build

---
+++ build/precache-manifest.5fe0476a44eefe559b4b4349d7674122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3c8289bf84737f3bedbb3f1b31341f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a1e374d67322e169ab",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "581278ae0049aadc4954",
+    "url": "/static/js/2.38cb3af7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38cb3af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a1e374d67322e169ab",
+    "url": "/static/js/main.e321a5d4.chunk.js"
+  },
+  {
+    "revision": "8661bc4c26bf8170c271",
+    "url": "/static/js/runtime-main.6e4f4f5c.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
